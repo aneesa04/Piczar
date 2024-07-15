6aabc479be8c60965ce7e012e9bc95b6c0649990
aneesa04 <aneesafatima238@gmail.com>
2024-07-15 18:14:13 +0530

    fixing ui

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -5,7 +5,7 @@ function Pagination({ data, nextPage, prevPage, page , totalPages}) {
   
 
   return (
-    <div className="buttons flex justify-center space-x-3 mb-6">
+    <div className="buttons flex justify-center space-x-3 my-6">
       <button className={`w-20  ${page<2 ? "bg-gray-400 opacity-90" : " bg-black text-white p-2 hover:bg-white hover:text-gray-700 hover:border hover:border-1 hover:border-gray-300"}`} disabled={page<2 ? true : false} onClick={prevPage}>
         Back
       </button>
