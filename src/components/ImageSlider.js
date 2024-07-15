6185415c9881c4320b8a39c40a942061d7bc13e5
aneesa04 <aneesafatima238@gmail.com>
2024-07-15 18:17:41 +0530

    fixing ui

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,28 +1,35 @@
-import React from 'react'
+import React from "react";
 
-function ImageSlider({data}) {
+function ImageSlider({ data }) {
   const array = data.tags.split(",");
-  //<a href="https://www.flaticon.com/free-icons/image-analysis" title="image analysis icons">Image analysis icons created by Dewi Sari - Flaticon</a>
+
   return (
     <div className="bg-white w-48 py-3 shadow-lg space-y-1 px-2 rounded-md mb-4 m-2">
-      <span>
-      </span>
-    <img src={data.webformatURL} alt="Images" className='rounded-sm '/>
-    <p className="font-bold text-purple-500 text-sm">{data.user}</p>
-    <ul className='text-xs'>
-      <li><strong>Views :</strong>{data.views}</li>
-      <li><strong>Downloads :</strong>{data.downloads}</li>
-      <li><strong>Likes :</strong>{data.likes}</li>
-    </ul>
+      <span></span>
+      <img src={data.webformatURL} alt="Images" className="rounded-sm " />
+      <p className="font-bold text-purple-500 text-sm">{data.user}</p>
+      <ul className="text-xs">
+        <li>
+          <strong>Views :</strong>
+          {data.views}
+        </li>
+        <li>
+          <strong>Downloads :</strong>
+          {data.downloads}
+        </li>
+        <li>
+          <strong>Likes :</strong>
+          {data.likes}
+        </li>
+      </ul>
 
-    <div className='space-x-1'>
-   {
-    array.map(item => (<span className='bg-gray-200 font-bold text-center text-gray-800 text-xxs rounded-xl px-2 py-0.5'>{`#${item}`}</span> ))
-   }
-
+      <div className="space-x-1">
+        {array.map((item) => (
+          <span className="bg-gray-200 font-bold text-center text-gray-800 text-xxs rounded-xl px-2 py-0.5">{`#${item}`}</span>
+        ))}
+      </div>
     </div>
-   </div>
-  )
+  );
 }
 
-export default ImageSlider
+export default ImageSlider;
